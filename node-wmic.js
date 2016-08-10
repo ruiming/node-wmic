8f04e2f432a5ad4debc725bc65ccae1e42d5b9e6
ruiming <ruiming.zhuang@gmail.com>
2016-08-10 17:38:20 +0800

    Rewrite in less code

--- node-wmic.js
+++ node-wmic.js
@@ -3,13 +3,13 @@ const exec = require('child_process').exec,
     iconv = require('iconv-lite');
 
 const cmd = {
-    'disk': 'wmic logicaldisk get /VALUE',
-    'cpu': 'wmic cpu get /VALUE',
-    'baseboard': 'wmic baseboard get /VALUE',
-    'bios': 'wmic bios get /VALUE',
-    'diskdrive': 'wmic diskdrive get /VALUE',
-    'os': 'wmic os get /VALUE',
-    'memorychip': 'wmic memorychip get /VALUE'
+    'disk': ['wmic logicaldisk get /VALUE', 40],
+    'cpu': ['wmic cpu get /VALUE', 57],
+    'baseboard': ['wmic baseboard get /VALUE', 29],
+    'bios': ['wmic bios get /VALUE', 31],
+    'diskdrive': ['wmic diskdrive get /VALUE', 51],
+    'os': ['wmic os get /VALUE', 64],
+    'memorychip': ['wmic memorychip get /VALUE', 36]
 };
 
 function wmicFormat(stdout, size) {
@@ -17,7 +17,7 @@ function wmicFormat(stdout, size) {
         let reg = /([^\r]+)=(.*)\r/g;
         let result = [];
         let one = {};
-        let length = stdout.match(/=/g).length;
+        let length = stdout.match(/=/g) && stdout.match(/=/g).length;
         for(let i=0; i<length/size; i++) {
             one = {};
             for(let j=0; j<size; j++) {
@@ -37,109 +37,20 @@ function wmicFormat(stdout, size) {
     });
 }
 
-module.exports = {
-    disk: function () {
-        return new Promise(function(resolve, reject) {
-            exec(cmd.disk, {encoding: 'GB2312'}, (err, stdout, stderr) => {
+let func = [];
+for (let key in cmd) {
+    func[key] = () => {
+        return new Promise((resolve, reject) => {
+            exec(cmd[key][0], {encoding: 'GB2312'}, (err, stdout, stderr) => {
                 if(err) {
-                    reject(stderr);
-                }
-                else {
-                    wmicFormat(iconv.decode(stdout, 'GB2312'), 40).then(result => {
+                    reject(stedrr);
+                } else {
+                    wmicFormat(iconv.decode(stdout, 'GB2312'), cmd[key][1]).then(result => {
                         resolve(result);
-                    });
+                    })
                 }
-            });
-        });
-    },
-
-    cpu: function () {
-        return new Promise((resolve, reject) => {
-            exec(cmd.cpu, (err, stdout, stderr) => {
-                if(err) {
-                    reject(err);
-                }
-                else {
-                    wmicFormat(stdout,57).then(result => {
-                        resolve(result);
-                    });
-                }
-            });
-        });
-    },
-
-    baseboard: function () {
-        return new Promise((resolve, reject) => {
-            exec(cmd.baseboard, (err, stdout, stderr) => {
-                if(err) {
-                    reject(err);
-                }
-                else {
-                    wmicFormat(stdout, 29).then(result => {
-                        resolve(result);
-                    });
-                }
-            });
-        });
-    },
-
-    bios: function () {
-        return new Promise((resolve, reject) => {
-            exec(cmd.bios, {encoding: 'GB2312'}, (err, stdout, stderr) => {
-                if (err) {
-                    reject(err);
-                }
-                else {
-                    wmicFormat(iconv.decode(stdout, 'GB2312'), 31).then(result => {
-                        resolve(result);
-                    });
-                }
-            });
-        });
-    },
-
-    os: function () {
-        return new Promise((resolve, reject) => {
-            exec(cmd.os, {encoding: 'GB2312'}, (err, stdout, stderr) => {
-                if (err) {
-                    reject(err);
-                }
-                else {
-                    wmicFormat(iconv.decode(stdout, 'GB2312'), 64).then(result => {
-                        resolve(result);
-                    });
-                }
-            });
-        });
-    },
-
-    memorychip: function () {
-        return new Promise((resolve, reject) => {
-            exec(cmd.memorychip, {encoding: 'GB2312'}, (err, stdout, stderr) => {
-                if (err) {
-                    reject(err);
-                }
-                else {
-                    wmicFormat(iconv.decode(stdout, 'GB2312'), 36).then(result => {
-                        resolve(result);
-                    });
-                }
-            });
-        });
-    },
-
-    diskdrive: function () {
-        return new Promise((resolve, reject) => {
-            exec(cmd.diskdrive, {encoding: 'GB2312'}, (err, stdout, stderr) => {
-                if(err) {
-                    reject(err);
-                }
-                else {
-                    wmicFormat(iconv.decode(stdout, 'GB2312'), 51).then(result => {
-                        resolve(result);
-                    });
-                }
-            });
-        });
+            })
+        })
     }
-};
+}
+module.exports = func;
